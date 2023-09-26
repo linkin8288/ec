293c73c8d168c65699a2a5beb86a4a4ca4b8db30
charlie <linkin8288@gmail.com>
2023-09-27 01:34:58 +0200

    delete export meta

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -3,7 +3,8 @@ import { getServerSession } from "next-auth";
 import { options } from "@/app/api/auth/[...nextauth]/options";
 import Navbar from "../components/Navbar";
 import AllCartProduct from "../components/AllCartProduct";
-import Allpurchased from "../components/AllPurchased";
+import Allpurchased from "../components/Allpurchased";
+
 
 type Props = {};
 
